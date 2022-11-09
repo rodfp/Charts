f889e27f55bd6a46d2571f26c9dba6b1fe20a8d0
Rodrigo Franco <rodrigo.perez@greenlight.me>
2022-11-08 20:28:30 -0800

    Check Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -7,7 +7,7 @@ let package = Package(
     platforms: [
           .iOS(.v12),
           .tvOS(.v12),
-          .macOS(.v10_12),
+          .macOS(.v10_13),
     ],
     products: [
         .library(
@@ -19,12 +19,13 @@ let package = Package(
             targets: ["Charts"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0")
+        .package(url: "https://github.com/GreenlightMe/greenlight-spm",
+                 branch: "main")
     ],
     targets: [
         .target(
             name: "Charts",
-            dependencies: [.product(name: "Algorithms", package: "swift-algorithms")]
+            dependencies: [.product(name: "Numerics", package: "greenlight-spm")]
         )
     ],
     swiftLanguageVersions: [.v5]
